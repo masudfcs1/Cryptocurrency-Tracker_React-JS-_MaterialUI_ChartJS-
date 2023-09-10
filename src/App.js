188a6eaa866ee987bbffae52bf97d72617cb03ae
masud rana <masudfcs1@gmail.com>
2023-09-10 21:38:47 +0600

    App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+// import { makeStyles } from "@material-ui/core";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+// import CoinPage from "./Pages/CoinPage";
+import Homepages from "./pages/Homepages";
+import Header from "./components/Header";
+import CoinPage from "./pages/CoinPage";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
-  return <div className="App">Hello World </div>;
-}
+const useStyles = makeStyles({
+  App: {
+    backgroundColor: "#2c3c4c",
+    color: "white",
+    minHeight: "100vh",
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepages} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>{" "}
+    </BrowserRouter>
+  );
+};
 
 export default App;
